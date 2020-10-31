7d51010ba62d0de967bcea37eaf812f2576a9293
aknm21 <amunt.valencia.visca.el.valencia@gmail.com>
2020-11-01 01:22:03 +0900

    add Font Awesome.

--- header.php
+++ header.php
@@ -14,8 +14,9 @@
 <html lang="ja">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 
 	<?php wp_head(); ?>
 </head>
@@ -23,11 +24,11 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'hardolass' ); ?></a>
+	<!-- <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'hardolass' ); ?></a> -->
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
+		<nav class="navbar navbar-expand-md fixed-top bg-white">
+			<a class="navbar-brand" href="/">			<?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
@@ -38,14 +39,31 @@
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
 			endif;
-			$hardolass_description = get_bloginfo( 'description', 'display' );
-			if ( $hardolass_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $hardolass_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+?></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Link</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="#">Disabled</a>
+					</li>
+				</ul>
+				<!-- <form class="form-inline mt-2 mt-md-0">
+					<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				</form> -->
+				<a class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</a>
+			</div>
+		</nav>
 
-		<nav id="site-navigation" class="main-navigation">
+		<!-- <nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hardolass' ); ?></button>
 			<?php
 			wp_nav_menu(
@@ -55,5 +73,5 @@
 				)
 			);
 			?>
-		</nav><!-- #site-navigation -->
+		</nav> --><!-- #site-navigation -->
 	</header><!-- #masthead -->
